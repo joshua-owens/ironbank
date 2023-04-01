b1062cf5fced7577b40759a16bc638645d71256c
Josh Owens <me@jowens.dev>
2023-03-31 22:35:17 -0500

    setting up saving bank items

--- bank/api.py
+++ bank/api.py
@@ -1,24 +1,27 @@
 from typing import List
+
+from django.core.exceptions import ObjectDoesNotExist
 from ninja import NinjaAPI
+from ninja.errors import ValidationError
+
 from .models import BankItem
-from .schemas import BankItemIn
+from .schemas import PlayerBankItemIn, BankItemsIn
 
 
 api = NinjaAPI()
 
 @api.post("/bank-items/")
-def create_bank_items(request, player_name: str, bank_items: List[BankItemIn]):
+def create_bank_items(request, data: BankItemsIn):
     # player, _ = Player.objects.get_or_create(name=player_name)
 
-    print(player_name)
-    print(bank_items)
 
-    for item_data in bank_items:
+    for item_data in data.bank_items:
+        print(item_data)
         # Get or create the BankItem instance based on the item_id
         try:
-            bank_item = BankItem.objects.get(item_id=item_data.item_id)
+            bank_item = BankItem.objects.get(item_id=item_data.id)
         except ObjectDoesNotExist:
-            bank_item = BankItem.objects.create(**item_data.dict())
+            bank_item = BankItem.objects.create(name=item_data.name, item_id=item_data.id, quantity=item_data.quantity)
 
         # Update the quantity and save the BankItem instance
         bank_item.quantity = item_data.quantity
@@ -28,3 +31,8 @@ def create_bank_items(request, player_name: str, bank_items: List[BankItemIn]):
         #player.bank_items.add(bank_item)
 
     return {"detail": "Bank items saved successfully"}
+
+@api.exception_handler(ValidationError)
+def custom_validation_errors(request, exc):
+    print(exc.errors)  # <--------------------- !!!!
+    return api.create_response(request, {"detail": exc.errors}, status=422)
--- bank/schemas.py
+++ bank/schemas.py
@@ -1,12 +1,17 @@
+from typing import List
+
 from pydantic import BaseModel, validator
 from bank.models import BankItem
 
 class BankItemIn(BaseModel):
-    item: list
+    id: int
+    quantity: int
+    name: str
 
-    @validator('item')
-    def validate_bank_item(cls, value):
-        # Implement your validation logic here, for example:
-        if not isinstance(value, BankItem):
-            raise ValueError("Invalid BankItem")
-        return value
+class BankItemsIn(BaseModel):
+    bank_items: List[BankItemIn]
+
+
+class PlayerBankItemIn(BaseModel):
+    bank_item: BankItemIn
+    player_name: str
